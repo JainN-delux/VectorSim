9ec93ff55860b0526507112708ad8ac718231310
Naitik Jain <naitik.jain07@gmail.com>
2025-05-06 12:18:35 -0400

    making a thingy

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 
+let message = "Waiting for Java...";
+
+function setup() {
+    createCanvas(400, 200);
+    fetch("http://localhost:8080/api/message")
+      .then(response => response.json())
+      .then(data => {
+        message = data.text;
+      });
+}
+
+function draw() {
+    background(220);
+    textSize(24);
+    textAlign(CENTER, CENTER);
+    text(message, width / 2, height / 2);
+}
